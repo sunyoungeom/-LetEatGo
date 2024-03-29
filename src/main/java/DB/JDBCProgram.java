eb54a24bf9d3c58f3d736232f82151dc620952f5
jaeuk <wodnr1029@nate.com>
2024-03-29 18:23:05 +0900

    DB 연결, DAO 생성

---
+++ src/main/java/DB/JDBCProgram.java
@@ -0,0 +1,24 @@
+package DB;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class JDBCProgram {
+	private static DataSource datasource = setupDataSource();
+
+	public static DataSource getInstance() {
+		System.out.println("...");
+		return datasource;
+	}
+	
+	private static DataSource setupDataSource() {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://192.168.0.107:3306/board");
+		ds.setUsername("team1");
+		ds.setPassword("root");
+		return ds;
+		
+	}
+}
---
+++ src/main/java/User/User.java
@@ -0,0 +1,28 @@
+package User;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String id;
+    private String email;
+    private String password;
+    private Timestamp join_date;
+    private Timestamp last_login;
+    private String name;
+    private String nickname;
+    private String mbti;
+    private String bloodtype;
+    private String identifynumber;
+    private String phonnumber;
+	
+    
+    
+}
