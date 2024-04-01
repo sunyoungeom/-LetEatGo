491325c205762f53f0bfbf59d6a32e0bbaac00f2
ilung2 <doqjjong4@naver.com>
2024-04-01 16:09:06 +0900

    일웅 커밋

--- src/main/java/websocket/ChatServer.java
+++ src/main/java/websocket/ChatServer.java
@@ -19,34 +19,34 @@ public class ChatServer {
 	private static Set<Session> clients 
 		= Collections.synchronizedSet(new HashSet<Session>());
 	
-//	@OnOpen // 클라이언트 접속 시 실행
-//	public void onOpen(Session session) {
-//		clients.add(session); // 세션 추가
-//		System.out.println("웹소켓 연결:" + session.getId());
-//	}
-//	
-//	@OnMessage // 메시지를 받으면 실행
-//	public void onMessage(String message, Session session) throws IOException {
-//		System.out.println("메시지 전송 : " + session.getId() + ":" + message);
-//		synchronized (clients) {
-//			for (Session c : clients) {
-//				if (!c.equals(session)) {
-//					// 단, 메시지를 보낸 클라이언트는 제외하고 전달
-//					c.getBasicRemote().sendText(message);
-//				}
-//			}
-//		}
-//	}
-//	
-//	@OnClose // 클라이언트와의 연결이 끊기면 실행
-//	public void onClose(Session session) {
-//		clients.remove(session);
-//		System.out.println("웹소켓 종료 : " + session.getId());
-//	}
-//	
-//	@OnError
-//	public void onError(Throwable e) {
-//		System.out.println("에러 발생");
-//		e.printStackTrace();
-//	}
+	@OnOpen // 클라이언트 접속 시 실행
+	public void onOpen(Session session) {
+		clients.add(session); // 세션 추가
+		System.out.println("웹소켓 연결:" + session.getId());
+	}
+	
+	@OnMessage // 메시지를 받으면 실행
+	public void onMessage(String message, Session session) throws IOException {
+		System.out.println("메시지 전송 : " + session.getId() + ":" + message);
+		synchronized (clients) {
+			for (Session c : clients) {
+				if (!c.equals(session)) {
+					// 단, 메시지를 보낸 클라이언트는 제외하고 전달
+					c.getBasicRemote().sendText(message);
+				}
+			}
+		}
+	}
+	
+	@OnClose // 클라이언트와의 연결이 끊기면 실행
+	public void onClose(Session session) {
+		clients.remove(session);
+		System.out.println("웹소켓 종료 : " + session.getId());
+	}
+	
+	@OnError
+	public void onError(Throwable e) {
+		System.out.println("에러 발생");
+		e.printStackTrace();
+	}
 }
--- src/main/webapp/WebSocket/MultiChatMain.jsp
+++ src/main/webapp/WebSocket/MultiChatMain.jsp
@@ -11,11 +11,12 @@
 		function chatWinOpen() {
 			var id = document.getElementById("chatId").value; // 사용자의 닉네임 가져오기
 			window.open("ChatWindow.jsp?chatId=" + id, "",
-					"width=400, height=400");
+					"width=400, height=450");
 		}
 	</script>
 	<h2>웹소켓 채팅 - 게시물에 대화참여 누르면 채팅방 참여하게만들어야함</h2>
-	대화명 :<input type="text" id="chatId" />
+	대화명 :<input type="text" id="chatId" /> 
+	<!-- id는 나중에 user.getNickname으로 변경한후 대화명 삭제 하기 -->
 	<button onclick="chatWinOpen();">채팅 참여</button>
 </body>
 
