61ea79b2707b5bbb2196a783066e82251a85a54a
dongso03 <dongso03@naver.com>
2024-04-18 16:09:21 +0900

    Merge branch 'main' of https://github.com/dongso03/-Final_Project.git into main

---
+++ src/main/java/user/UserTagServlet.java
@@ -0,0 +1,9 @@
+package user;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/userTag")
+public class UserTagServlet extends HttpServlet{
+
+}
